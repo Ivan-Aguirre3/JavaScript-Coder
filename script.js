4c3ba401ec488aeb1a49ee125f710e290fb564da
Ivan-Aguirre3 <109114053+Ivan-Aguirre3@users.noreply.github.com>
2022-09-26 17:05:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let result  = document.getElementById("result");
+
+function input(num){
+    let number = result.value;
+    result.value = number + num;
+}
+
+function calc(){
+    if(result.value != ""){
+        let result2  = result.value;
+        result.value = eval(result2)
+    } else{
+        alert("ERROR")
+    }
+}
+
+function reset(){
+    result.value = "";
+}
+
+function del(){
+    let result2  = result.value;
+    result.value = result2.substring(0, result2.length - 1);
+}
